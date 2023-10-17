407001df0da64e534ca570782de04a7c2851b456
Sayan Abibillaev <sayan@Sayans-MacBook-Air.local>
2023-10-17 21:26:48 +0600

    tgbot is done

--- tg_bot/main_bot.py
+++ tg_bot/main_bot.py
@@ -4,6 +4,7 @@ import json
 from decouple import config
 
 bot = telebot.TeleBot(config('TELEGRAM_BOT_TOKEN'))
+current_page = 1
 
 
 @bot.message_handler(commands=['start'])
@@ -14,25 +15,36 @@ def send_welcome(message):
     bot.reply_to(message, 'Hello, AniSlime API welcomes you, glad to help you!', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'show_anime')
-def send_title_anime(call):
-    res = requests.get('http://34.89.235.149/api/v1/title/')
+def load_anime_from_page(page, chat_id):
+    res = requests.get(f'http://34.89.235.149/api/v1/title/?page={page}')
     data = json.loads(res.content)
     data_next = data.get('next')
-    data_previous = data.get('previous')
     titles = data.get('results')
 
-    titles_sorted_by_views = sorted(titles, key=lambda x: x.get("views", 0), reverse=True)
-
-    for index, i in enumerate(titles_sorted_by_views, 1):
-        bot.send_message(call.message.chat.id, f'Аниме по популярности {index}: \n{i.get("name")}')
+    for index, anime in enumerate(titles, 1):
+        anime_name = anime.get("name")
+        bot.send_message(chat_id, f'{anime_name}')
 
     if data_next:
         markup = telebot.types.InlineKeyboardMarkup()
-        item = telebot.types.InlineKeyboardButton("Click to see next page of anime", callback_data='show_anime')
+        item = telebot.types.InlineKeyboardButton("Click to see next page of anime", callback_data='show_next_page')
         markup.add(item)
+        bot.send_message(chat_id, "Доступна следующая страница с аниме:", reply_markup=markup)
 
-        bot.send_message(call.message.chat.id, "Доступна следующая страница с аниме:", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'show_anime')
+def send_title_anime(call):
+    global current_page
+
+    load_anime_from_page(current_page, call.message.chat.id)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'show_next_page')
+def show_next_page(call):
+    global current_page
+    current_page += 1
+
+    load_anime_from_page(current_page, call.message.chat.id)
 
 
 if __name__ == '__main__':
