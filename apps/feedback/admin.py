e6ca447739a917b885ad92b080cf0a6b461ce4e2
ovaomnis <ovaomnis@gmail.com>
2023-10-24 15:09:01 +0600

    admin added

--- apps/feedback/admin.py
+++ apps/feedback/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
--- apps/title/admin.py
+++ apps/title/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TitleYear)
+class TitleYearAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    pass
